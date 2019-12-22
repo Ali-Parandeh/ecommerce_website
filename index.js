37341cb38f9d9b41186ff9b474a913ee7d81f75a
Ali Parandeh <Ali.Parandeh@outlook.com>
2019-12-22 17:39:17 +0000

    Add signup, signout, signin routes

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 // npm run dev
 // Run the above command in the terminal to start the server.
 const express = require("express");
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session')
 const usersRepo = require('./repositories/users');
 
 const app = express()
 
 // bodyParser is the middleware being called before the callback function being invoked by the routers.
-// Instead of specifying the bodyParser middleware for each route, we use app.use to apply it to all routes
+// Instead of specifying the bodyParser middleware for each route, we use app.use to apply it to all routes.
 app.use(bodyParser.urlencoded({extended: true}))
+
+// CookieSession is a middleware like BodyParser. To use it in all routes, we can use app.use
+// Note: Keys options passed in to CookieSession is used to encrypt content of the cookies stored on user's browser.
+// Note: CookieSession library adds an additional property to the req object passed in to routes below - req.session.
+app.use(cookieSession({keys: ['kjdhasjdhajskhd']}));
 
 
 // req: incoming information to server from the browser. Use req when receiving information from the user
 // res: outgoing response from the server to the browser. Use res to send info back to the user or interact with the browser.
 // Listen for GET requests on the root route (/)
-app.get("/", (req, res) => 
+app.get("/signup", (req, res) => 
 {
     // You can send a piece of text that is html using `` which will be rendered by the browser.
     // Note: The default method for form submission is GET so you need to overwrite this with a POST request.
     res.send(`
         <div>
+            Your id is: ${req.session.userId}
             <form method="POST">
                 <input name="email" placeholder="email">
                 <input name="password" placeholder="password">
@@ -69,7 +76,7 @@ app.get("/", (req, res) =>
 
 // Post route of the root rout method to server first, server runs the appropriate callback, then chunk by chunk
 // the browser sends the rest of the information to the server while receiving confirmation of each chunk
-app.post('/', async (req, res) => 
+app.post('/signup', async (req, res) => 
 {
     const {email, password, passwordConfirmation} = req.body;
 
@@ -93,9 +100,54 @@ app.post('/', async (req, res) =>
     const user = await usersRepo.create({email, password});
 
     // Store the id of the user inside the user cookie.
+    req.session.userId = user.id;
 
     res.send('Account Created');
 });
+
+// Sign out route
+app.get('/signout', (req, res) => 
+{
+    req.session = null
+    res.send('You are logged out!');
+});
+
+// Sign in route (GET)
+app.get('/signin', (req, res) => 
+{
+    res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email">
+            <input name="password" placeholder="password">
+            <button>Sign in</button>
+        </form>
+    </div> 
+    `);
+});
+
+// Sign in route (POST)
+app.post('/signin', async (req, res) => 
+{
+    const {email, password} = req.body;
+    const user = await usersRepo.getOneBy({email});
+
+    if(!user)
+    {
+        return res.send('Email not found');
+    }
+
+    if(user.password !== password)
+    {
+        return res.send('Invalid password');
+    }
+
+    req.session.userId = user.id;
+
+    res.send('You are signed in!');
+
+});
+
 
 // Need to start a server and listen on a port (3000 here) to access the routes above
 app.listen(3001, () => 
