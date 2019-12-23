5e4291346712e986f728c84354da7f6e03357a7c
Ali Parandeh <Ali.Parandeh@outlook.com>
2019-12-23 11:08:27 +0000

    Implement layout views

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,21 +1,17 @@
+const layout = require('../layout');
+
 // Exporting template with req object. {req} destructures the argument.
 module.exports = ({req})=> 
 {
-    return `
-    <!DOCTYPE html>
-    <html>
-        <head></head>
-        <body>
-            <div>
-                Your id is: ${req.session.userId}
-                <form method="POST">
-                    <input name="email" placeholder="email">
-                    <input name="password" placeholder="password">
-                    <input name="passwordConfirmation" placeholder="password confirmation">
-                    <button>Sign up</button>
-                </form>
-            </div>
-        </body>
-    </html>
-    `;
+    return layout({content:`        
+        <div>
+            Your id is: ${req.session.userId}
+            <form method="POST">
+                <input name="email" placeholder="email">
+                <input name="password" placeholder="password">
+                <input name="passwordConfirmation" placeholder="password confirmation">
+                <button>Sign up</button>
+            </form>
+        </div>
+    `});
 }
---
+++ views/admin/layout.js
@@ -0,0 +1,13 @@
+module.exports = ({content}) =>
+{
+    return `
+        <!DOCTYPE hmtl>
+        <html>
+            <head>
+            </head>
+            <body>
+                ${content}
+            </body>
+        </html>
+    `;
+};
