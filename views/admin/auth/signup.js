82805cdb1be21b95ce76e1520670a3bf7f6474dd
Ali Parandeh <Ali.Parandeh@outlook.com>
2019-12-24 14:10:52 +0000

    Add signup validation checks

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,15 +1,28 @@
 const layout = require("../layout");
+// helper function for error handling
+const getError = (errors, prop) => {
+  // prop === 'email || 'password' || 'passwordConfirmation'
+  try {
+    // errors.mapped() returns an object with each error mapped as key
+    return errors.mapped()[prop].msg;
+  } catch (err) {
+    return "";
+  }
+};
 
 // Exporting template with req object. {req} destructures the argument.
-module.exports = ({ req }) => {
+module.exports = ({ req }, errors) => {
   return layout({
     content: `        
         <div>
             Your id is: ${req.session.userId}
             <form method="POST">
                 <input name="email" placeholder="email">
+                ${getError(errors, "email")}
                 <input name="password" placeholder="password">
+                ${getError(errors, "password")}
                 <input name="passwordConfirmation" placeholder="password confirmation">
+                ${getError(errors, "passwordConfirmation")}
                 <button>Sign up</button>
             </form>
         </div>
