4ae2916e05e32c960876426b5315696c34ed8da0
Ali Parandeh <Ali.Parandeh@outlook.com>
2019-12-26 14:11:11 +0000

    Add comments

--- views/helpers.js
+++ views/helpers.js
@@ -1,6 +1,6 @@
 module.exports = {
   getError(errors, prop) {
-    // prop === 'email || 'password' || 'passwordConfirmation'
+    // prop === 'email' || 'password' || 'passwordConfirmation'
     try {
       // errors.mapped() returns an object with each error mapped as key
       return errors.mapped()[prop].msg;
