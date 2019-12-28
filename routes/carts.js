deaab768b8ca72eaab429254ceb445d801d6a007
Ali Parandeh <Ali.Parandeh@outlook.com>
2019-12-28 16:01:20 +0000

    Add cart item deletion route handler

--- routes/carts.js
+++ routes/carts.js
@@ -33,7 +33,7 @@ router.post("/cart/products", async (req, res) => {
     items: cart.items
   });
 
-  res.send("Product added to cart");
+  res.redirect("/cart");
 });
 
 // Recieve a GET request to show all items in a cart
@@ -54,6 +54,13 @@ router.get("/cart", async (req, res) => {
 });
 
 // Recieve a POST request to delete and item for a cart
-router.post("/cart/products/:id/delete", async (req, res) => {});
+router.post("/cart/products/delete", async (req, res) => {
+  const { itemId } = req.body;
+  const cart = await cartsRepo.getOne(req.session.cartId);
+  const items = cart.items.filter(item => item.id !== itemId);
+  await cartsRepo.update(req.session.cartId, { items });
+
+  res.redirect("/cart");
+});
 
 module.exports = router;
